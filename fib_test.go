88692e402f9684d28556f127f2ede8f3135f7db3
wwwnay <ywilling@outlook.com>
2022-04-13 17:05:29 +0800

    Update fib_test.go

--- fib_test.go
+++ fib_test.go
@@ -1,9 +1,12 @@
-package fib
+package main
 
-import (
-	"testing"
-)
+import "testing"
 
+func BenchmarkFib(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fib(30) // run fib(30) b.N times
+	}
+}
 func BenchmarkFib10(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		var _ = Fib(10)
@@ -12,6 +15,6 @@ func BenchmarkFib10(b *testing.B) {
 
 func BenchmarkFib20(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		var _ = Fib(30)
+		var _ = Fib(20)
 	}
 }
