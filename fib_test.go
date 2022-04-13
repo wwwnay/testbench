b086c48f38c212349b80a036b0aa2723f57940e8
wwwnay <ywilling@outlook.com>
2022-04-13 17:18:55 +0800

    Update fib_test.go

--- fib_test.go
+++ fib_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func BenchmarkFib(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		fib(30) // run fib(30) b.N times
+		Fib(30) // run fib(30) b.N times
 	}
 }
 func BenchmarkFib10(b *testing.B) {
